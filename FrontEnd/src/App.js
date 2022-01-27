6873d842082beb29f5e3792ecfdaf9f0a781be7e
19santosh99 <santoshmaturi99@gmail.com>
2022-01-27 18:43:25 -0500

    Added( Accessing GeoLocation from the user )

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,5 +1,62 @@
-function App() {
-  return <h1>My App!</h1>;
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: null,
+      longitude: null,
+    };
+    this.getLocation = this.getLocation.bind(this);
+    this.getCoordinates = this.getCoordinates.bind(this);
+  }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this.getCoordinates,
+        this.handleLocationError
+      );
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
+  getCoordinates(position) {
+    this.setState({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  }
+
+  handleLocationError(error) {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        alert("User denied the request for Geolocation.");
+        break;
+      case error.POSITION_UNAVAILABLE:
+        alert("Location information is unavailable.");
+        break;
+      case error.TIMEOUT:
+        alert("The request to get user location timed out.");
+        break;
+      case error.UNKNOWN_ERROR:
+        alert("An unknown error occurred.");
+        break;
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.getLocation()}
+        {/* <button onClick={this.getLocation}>Get Coordinates</button> */}
+        <p>Latitude: {this.state.latitude}</p>
+        <p>Longitude: {this.state.longitude}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
