1e154daebc46fa9a9bb9ff8e088f88d68a6f75f8
Medha Naik <medha.naik@ufl.edu>
2022-01-28 02:03:39 -0500

    Web app works now

---
+++ Backend/go.mod
@@ -0,0 +1,31 @@
+module example.com/partyholic
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.7 // indirect
+	github.com/go-playground/validator/v10 v10.10.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-sqlite3 v1.14.10 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/ugorji/go v1.2.6 // indirect
+	golang.org/x/crypto v0.0.0-20220126234351-aa10faf2a1f8 // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
+
+require (
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	geo v0.0.0
+	database v0.0.0
+
+)
+
+replace geo v0.0.0 => ./geo/
+replace database v0.0.0 => ./database/
+
+
--- Backend/main.go
+++ Backend/main.go
@@ -1,12 +1,57 @@
 package main
 
 import (
-	"./pkg/utils/geo"
-	"./pkg/server"
+	"net/http"
+	"database/database"
+	"geo/geo"
+	"github.com/gin-gonic/gin"
 )
 
+func getPersons(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "getPersons Called"})
+}
+
+func getPersonById(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{"message": "getPersonById " + id + " Called"})
+}
+
+func addPerson(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "addPerson Called"})
+}
+
+func updatePerson(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "updatePerson Called"})
+}
+
+func deletePerson(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{"message": "deletePerson " + id + " Called"})
+}
+
+func options(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "options Called"})
+}
+
 func main() {
-	// fmt.Println("Server starting...")
+	r := gin.Default()
+
+	// API v1
+	v1 := r.Group("/api/v1")
+	{
+		v1.GET("person", getPersons)
+		v1.GET("person/:id", getPersonById)
+		v1.POST("person", addPerson)
+		v1.PUT("person/:id", updatePerson)
+		v1.DELETE("person/:id", deletePerson)
+		v1.OPTIONS("person", options)
+	}
+
 	geo.GeoAddress("309 Sw 16th ave apt 219, Gainesville, FL 32608")
-	server.Database_setup()
+	database.Database_setup()
+
+	// By default it serves on :8080 unless a
+	// PORT environment variable was defined.
+	r.Run()
+
 }
