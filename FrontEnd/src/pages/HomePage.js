a71a637476ca5d0bd42be1a025e9d86f9ed79796
19santosh99 <santoshmaturi99@gmail.com>
2022-01-28 10:08:08 -0500

    Made Code Readable

--- FrontEnd/src/pages/HomePage.js
+++ FrontEnd/src/pages/HomePage.js
@@ -1,33 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Image1 from '../assets/Image1.jpg'
-import Image2 from '../assets/Image2.jpg'
+import Image1 from "../assets/Image1.jpg";
+import Image2 from "../assets/Image2.jpg";
 
 function Homepage() {
-    return (
-      <div className='home'>
-        <header>
-          <p className='pageHeader'>Welcome to PartyHolic!!!!!</p>
-        </header>
-  
-        <main>
-        <Link to='/about'>
-            <p className='homeCategoryName'>About page</p>
-          </Link>
-          <p className='homeCategoryHeading'>Party categories</p>
-          <div className='homeCategories'>
+  return (
+    <div className="home">
+      <header>
+        <p className="pageHeader">Welcome to PartyHolic!!!!!</p>
+      </header>
 
-              <img
-                src={Image1}
-                alt='rent'
-                className='homeCategoryImg'
-              />
-            
-            
-          </div>
-        </main>
-      </div>
-    )
-  }
-  
-  export default Homepage
+      <main>
+        <Link to="/about">
+          <p className="homeCategoryName">About page</p>
+        </Link>
+        <p className="homeCategoryHeading">Party categories</p>
+        <div className="homeCategories">
+          <img src={Image1} alt="rent" className="homeCategoryImg" />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Homepage;
