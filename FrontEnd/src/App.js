73277d4289bf26eda0f840cd61c961f18e20599d
19santosh99 <santoshmaturi99@gmail.com>
2022-01-31 17:30:26 -0500

    changed class to function for (user location)

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -8,67 +8,45 @@ import Services from './pages/services';
 import Contact from './pages/contact';
 import SignUp from './pages/signup';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      latitude: null,
-      longitude: null,
-    };
-    this.getLocation = this.getLocation.bind(this);
-    this.getCoordinates = this.getCoordinates.bind(this);
-  }
 
-  getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        this.getCoordinates,
-        this.handleLocationError
-      );
-    } else {
-      alert("Geolocation is not supported by this browser.");
-    }
-  }
+function App(props) {
+  const[latitude, setLatitude] = useState('');
+  const[longitude, setLongitude] = useState('');
 
-  getCoordinates(position) {
-    this.setState({
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude,
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
     });
-  }
+  },[]);
 
-  handleLocationError(error) {
-    switch (error.code) {
-      case error.PERMISSION_DENIED:
-        alert("User denied the request for Geolocation.");
-        break;
-      case error.POSITION_UNAVAILABLE:
-        alert("Location information is unavailable.");
-        break;
-      case error.TIMEOUT:
-        alert("The request to get user location timed out.");
-        break;
-      case error.UNKNOWN_ERROR:
-        alert("An unknown error occurred.");
-        break;
-    }
-  }
-
-  render() {
-    return (
-      <Router>
-        {this.getLocation()}
-        <Navbar />
-        <Routes>
-          <Route path='/' exact element={<Home />} />
-          <Route path='/about' element={<About/>} />
-          <Route path='/services' element={<Services/>} />
-          <Route path='/contact-us' element={<Contact/>} />
-          <Route path='/sign-up' element={<SignUp/>} />
-        </Routes>
-      </Router>
-    );
-  }
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/services' element={<Services/>} />
+        <Route path='/contact-us' element={<Contact/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+      </Routes>
+    </Router>
+  );
 }
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       latitude: null,
+//       longitude: null,
+//     };
+//     this.getLocation = this.getLocation.bind(this);
+//     this.getCoordinates = this.getCoordinates.bind(this);
+//   }
+
+// }
 
 export default App;
