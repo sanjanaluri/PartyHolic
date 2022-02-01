e8000a1c6464c20354f6083929069f978778904a
Sanjana Aluri <alurisanjana9@gmail.com>
2022-02-01 11:32:08 -0500

    updated after conflicts

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,66 +1,14 @@
 import React from "react";
-import Homepage from "./pages/HomePage";
-import About from "./pages/AboutPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-<<<<<<< Updated upstream
-=======
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import About from './pages/about';
 import Contact from './pages/contact';
 import SignUp from './pages/signup';
 import PartyList from "./pages/PartyList";
->>>>>>> Stashed changes
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      latitude: null,
-      longitude: null,
-    };
-    this.getLocation = this.getLocation.bind(this);
-    this.getCoordinates = this.getCoordinates.bind(this);
-  }
 
-<<<<<<< Updated upstream
-  getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        this.getCoordinates,
-        this.handleLocationError
-      );
-    } else {
-      alert("Geolocation is not supported by this browser.");
-    }
-  }
-
-  getCoordinates(position) {
-    this.setState({
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude,
-    });
-  }
-
-  handleLocationError(error) {
-    switch (error.code) {
-      case error.PERMISSION_DENIED:
-        alert("User denied the request for Geolocation.");
-        break;
-      case error.POSITION_UNAVAILABLE:
-        alert("Location information is unavailable.");
-        break;
-      case error.TIMEOUT:
-        alert("The request to get user location timed out.");
-        break;
-      case error.UNKNOWN_ERROR:
-        alert("An unknown error occurred.");
-        break;
-    }
-  }
-=======
 function App(props) {
   return (
     <Router>
@@ -76,33 +24,5 @@ function App(props) {
   );
 }
 
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       latitude: null,
-//       longitude: null,
-//     };
-//     this.getLocation = this.getLocation.bind(this);
-//     this.getCoordinates = this.getCoordinates.bind(this);
-//   }
->>>>>>> Stashed changes
-
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <Routes>
-            <Route exact path="/" element={<Homepage />} />
-            <Route exact path="/about" element={<About />} />
-          </Routes>
-        </Router>
-        {this.getLocation()}
-        <p>Latitude: {this.state.latitude}</p>
-        <p>Longitude: {this.state.longitude}</p>
-      </div>
-    );
-  }
-}
 
 export default App;
