88854350cb53e424b70c900f838de0adbff6f14b
19santosh99 <santoshmaturi99@gmail.com>
2022-02-25 18:01:59 -0500

    fixed user location bug

--- FrontEnd/src/context/EventsContext.js
+++ FrontEnd/src/context/EventsContext.js
@@ -5,53 +5,49 @@ const EventsContext = createContext();
 export const EventsProvider = ({ children }) => {
   const [eventsList, setEventsList] = useState(null);
 
-  const[gotLocation, setGotLocation] = useState(false)
+  const [gotLocation, setGotLocation] = useState(false);
   const [coordinates, setcoordinates] = useState({
-	user_id: '1',
-  location: {
-  	latitude: parseInt(''),
-    longitude: parseInt('')
-   },
-  radius_meters: 50
-});
-
-useEffect(() => {
-  navigator.geolocation.getCurrentPosition(function (position) {
-    setGotLocation(true);
-    setcoordinates((prevState) => {
-      return {
-        ...prevState,
-        location: {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude
-        }
-      };
-    });
-    console.log("Latitude is :", position.coords.latitude);
-    console.log("Longitude is :", position.coords.longitude);
+    user_id: "1",
+    location: {
+      latitude: parseInt(""),
+      longitude: parseInt(""),
+    },
+    radius_meters: 50,
   });
-}, []);
 
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setcoordinates((prevState) => {
+        return {
+          ...prevState,
+          location: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          },
+        };
+      });
+      setGotLocation(true);
+      console.log(coordinates.location.latitude);
+    });
+  }, []);
 
-
-useEffect(() => {
-  fetch('http://localhost:8080/api/parties',  {
-    method:'POST',
-    headers:{ 'Content-Type':'appplication/json'},
-    body:JSON.stringify(coordinates)
-}
-).then(res =>{
-    return res.json();
-  }).then(data =>{
-    console.log(data)
-    setEventsList(data);
-  })
-},[gotLocation]);
-
-
+  useEffect(() => {
+    fetch("http://localhost:8080/api/parties", {
+      method: "POST",
+      headers: { "Content-Type": "appplication/json" },
+      body: JSON.stringify(coordinates),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setEventsList(data);
+      });
+  }, [gotLocation]);
 
   return (
-    <EventsContext.Provider value={{eventsList}}>
+    <EventsContext.Provider value={{ eventsList }}>
       {children}
     </EventsContext.Provider>
   );
