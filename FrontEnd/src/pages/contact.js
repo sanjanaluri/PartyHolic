056b4e37ed0caa4ad504fa6663f20735172e8fd9
19santosh99 <santoshmaturi99@gmail.com>
2022-03-03 22:21:40 -0500

    Written Tests Zipcode Component and Contact Component

--- FrontEnd/src/pages/contact.js
+++ FrontEnd/src/pages/contact.js
@@ -2,6 +2,9 @@ import React from "react";
 import { width } from "tailwindcss/defaultTheme";
 
 const Contact = () => {
+  const handleSubmit = () => {
+    console.log("Form Submitted");
+  };
   return (
     <div>
       <div
@@ -12,82 +15,84 @@ const Contact = () => {
           height: "100vh",
           marginTop: "40px",
         }}
-
         className="bg-img"
       >
-        <form class="w-full max-w-lg bg-white rounded-md text-center px-5 py-5">
-          <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+        <form
+          className="w-full max-w-lg bg-white rounded-md text-center px-5 py-5"
+          onSubmit={handleSubmit}
+        >
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
               <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-first-name"
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-first-name"
               >
                 First Name
               </label>
               <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                 id="grid-first-name"
                 type="text"
                 placeholder="Santosh"
               />
-              <p class="text-red-500 text-xs italic">
+              <p className="text-red-500 text-xs italic">
                 Please fill out this field.
               </p>
             </div>
-            <div class="w-full md:w-1/2 px-3">
+            <div className="w-full md:w-1/2 px-3">
               <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-last-name"
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-last-name"
               >
                 Last Name
               </label>
               <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 id="grid-last-name"
                 type="text"
                 placeholder="Maturi"
               />
             </div>
           </div>
-          <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full px-3">
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
               <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-password"
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-password"
               >
                 E-mail
               </label>
               <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 id="email"
                 type="email"
               />
             </div>
           </div>
-          <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full px-3">
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
               <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-password"
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-password"
               >
                 Message
               </label>
               <textarea
-                class=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+                className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
                 id="message"
               ></textarea>
             </div>
           </div>
-          <div class="md:flex md:items-center">
-            <div class="items-center">
+          <div className="md:flex md:items-center">
+            <div className="items-center" role="dialog">
               <button
-                class="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                className="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
                 type="button"
               >
                 Send
               </button>
             </div>
-            <div class="md:w-2/3"></div>
+            <div className="md:w-2/3"></div>
           </div>
         </form>
       </div>
---
+++ FrontEnd/src/pages/contact.test.js
@@ -0,0 +1,26 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+import { ReactDOM } from "react";
+import Contact from "./contact";
+import { isTSAnyKeyword } from "@babel/types";
+
+test("Checking Zipcode component", () => {
+  //   const element = document.createElement("div");
+  //   expect(element).not.toBeNull();
+  render(<Contact />);
+  const firstName = screen.getByLabelText("First Name");
+  expect(firstName.value).toMatch("");
+  fireEvent.change(firstName, { target: { value: "Unit Testing" } });
+  expect(firstName.value).toMatch("Unit Testing");
+});
+
+test("user should be able to submit the contact form", () => {
+  const mockFunction = jest.fn();
+  render(<Contact handleSubmit={mockFunction} />);
+  const buttonNode = screen.getByRole("dialog");
+  fireEvent.submit(buttonNode);
+});
