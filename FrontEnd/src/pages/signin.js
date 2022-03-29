101b681066b4b1b7ecea7b1221304a7ee81687a3
19santosh99 <santoshmaturi99@gmail.com>
2022-03-29 16:10:32 -0400

    Added functionality to the form Buttons

--- FrontEnd/src/pages/signin.js
+++ FrontEnd/src/pages/signin.js
@@ -1,6 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { values, size } from "lodash";
+import { toast } from "react-toastify";
 
 const SignIn = () => {
+  const { setRefreshCheckLogin } = props;
+  const [formData, setFormData] = useState(initialFormValue());
+  const [signInLoading, setSignInLoading] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+
+    let validationCount = 0;
+    values(formData).some((value) => {
+      value && validationCount++;
+      return null;
+    });
+
+    //validate the input size entered by matching args
+    if (size(formData) !== validationCount) {
+      toast.warning("Enter all details in the mentioned fields ");
+    } else {
+      if (!isEmailValid(formData.email)) {
+        toast.warning("Invalid Email Entered");
+      } else {
+        setSignInLoading(true);
+        signInApi(formData)
+          .then((response) => {
+            if (response.message) {
+              toast.warning(response.message);
+            } else {
+              setTokenApi(response.Token);
+              setRefreshCheckLogin(true);
+            }
+          })
+          .catch(() => {
+            toast.error("Server error, please try again later");
+          })
+          .finally(() => {
+            setSignInLoading(false);
+          });
+        //toast.success("Gator Login Successful");
+      }
+    }
+
+    console.log(validationCount);
+  };
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   return (
     <section class="flex flex-col md:flex-row h-full items-center">
       <div
@@ -12,14 +62,15 @@ const SignIn = () => {
             Log in to your account
           </h1>
 
-          <form class="mt-6">
+          <form class="mt-6" onSubmit={onSubmit} onChange={onChange}>
             <div>
               <label class="block text-gray-700">Email Address</label>
               <input
                 type="email"
-                name=""
-                id=""
+                name="email"
+                id="emailID"
                 placeholder="Enter Email Address"
+                defaultValue={formData.email}
                 class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-purple-500 focus:bg-white focus:outline-none"
                 autofocus
                 autocomplete
@@ -31,10 +82,10 @@ const SignIn = () => {
               <label class="block text-gray-700">Password</label>
               <input
                 type="password"
-                name=""
+                name="password"
                 id=""
+                defaultValue={formData.password}
                 placeholder="Enter Password"
-                minlength="6"
                 class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-purple-500
                   focus:bg-white focus:outline-none"
                 required
@@ -48,6 +99,7 @@ const SignIn = () => {
             </div>
 
             <button
+              role="submitForm"
               type="submit"
               class="w-full block bg-purple-500 hover:bg-purple-400 focus:bg-purple-400 text-white font-semibold rounded-lg
                 px-4 py-3 mt-6"
@@ -72,14 +124,21 @@ const SignIn = () => {
         </div>
       </div>
       <div class="bg-purple-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
-            <img
-              src="https://images.unsplash.com/photo-1527529482837-4698179dc6ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3870&q=80"
-              alt=""
-              class="w-full h-full object-cover"
-            ></img>
-          </div>
+        <img
+          src="https://images.unsplash.com/photo-1527529482837-4698179dc6ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3870&q=80"
+          alt=""
+          class="w-full h-full object-cover"
+        ></img>
+      </div>
     </section>
   );
 };
+
+function initialFormValue() {
+  return {
+    email: "",
+    password: "",
+  };
+}
 
 export default SignIn;
