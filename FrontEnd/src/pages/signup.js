f7eb2a6df74783928bf417a7104f032fa26245ef
Sanjana Aluri <alurisanjana9@gmail.com>
2022-03-29 14:51:37 -0400

    Added image and adjusted signup form alignment

--- FrontEnd/src/pages/signup.js
+++ FrontEnd/src/pages/signup.js
@@ -8,13 +8,13 @@ const SignUp = () => {
           flex items-center justify-center"
       >
         <div class="w-full h-full">
-          <h1 class="text-xl md:text-2xl font-bold leading-tight mt-12">
+          <h1 class="text-xl md:text-2xl font-bold leading-tight mt-4">
             Register Today
           </h1>
 
           <form class="mt-6">
-
-          <div class="mt-2">
+          <div className="flex flex-wrap -mx-3 mb-6">
+          <div class="mt-2 w-full md:w-1/2 px-3 mb-6 md:mb-0">
               <label class="block text-gray-700">First Name</label>
               <input
                 type="text"
@@ -22,13 +22,13 @@ const SignUp = () => {
                 id=""
                 placeholder="Enter First Name"
                 minlength="6"
-                class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-purple-500
+                class="w-full px-4 py-1 rounded-lg bg-gray-200 mt-2 border focus:border-purple-500
                   focus:bg-white focus:outline-none"
                 required
               ></input>
             </div>
 
-            <div class="mt-2">
+            <div class="mt-2 w-full md:w-1/2 px-3">
               <label class="block text-gray-700">Last Name</label>
               <input
                 type="text"
@@ -36,12 +36,12 @@ const SignUp = () => {
                 id=""
                 placeholder="Enter Last Name"
                 minlength="6"
-                class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-purple-500
+                class="w-full px-4 py-1 rounded-lg bg-gray-200 mt-2 border focus:border-purple-500
                   focus:bg-white focus:outline-none"
                 required
               ></input>
             </div>
-
+            </div>
             <div class="mt-2">
               <label class="block text-gray-700">Email Address</label>
               <input
@@ -49,7 +49,7 @@ const SignUp = () => {
                 name=""
                 id=""
                 placeholder="Enter Email Address"
-                class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-purple-500 focus:bg-white focus:outline-none"
+                class="w-full px-4 py-1 rounded-lg bg-gray-200 mt-2 border focus:border-purple-500 focus:bg-white focus:outline-none"
                 autofocus
                 autocomplete
                 required
@@ -64,7 +64,7 @@ const SignUp = () => {
                 id=""
                 placeholder="Enter Password"
                 minlength="6"
-                class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-purple-500
+                class="w-full px-4 py-1 rounded-lg bg-gray-200 mt-2 border focus:border-purple-500
                   focus:bg-white focus:outline-none"
                 required
               ></input>
@@ -73,12 +73,12 @@ const SignUp = () => {
             <div class="mt-2">
               <label class="block text-gray-700">Gender</label>
               <div class="relative">
-              <select class="block appearance-none w-full bg-gray-200 border focus:border-purple-500 focus:bg-white focus:outline-none text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+              <select class="block appearance-none w-full bg-gray-200 border focus:border-purple-500 focus:bg-white focus:outline-none text-gray-700 py-1 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
                 <option>Male</option>
                 <option>Female</option>
                 
               </select>
-              <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+              <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 py-1 text-gray-700">
                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
               </div>
             </div>
@@ -91,13 +91,14 @@ const SignUp = () => {
                 id=""
                 placeholder="Enter Full Address"
                 minlength="6"
-                class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-purple-500
+                class="w-full px-4 py-1 rounded-lg bg-gray-200 mt-2 border focus:border-purple-500
                   focus:bg-white focus:outline-none"
                 required
               ></input>
             </div>
 
-            <div class="mt-2">
+            <div className="flex flex-wrap -mx-3 mb-6">
+            <div class="mt-2 w-full md:w-1/2 px-3 mb-6 md:mb-0">
               <label class="block text-gray-700">City </label>
               <input
                 type="text"
@@ -105,13 +106,13 @@ const SignUp = () => {
                 id=""
                 placeholder="Enter City"
                 minlength="6"
-                class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-purple-500
+                class="w-full px-4 py-1 rounded-lg bg-gray-200 mt-2 border focus:border-purple-500
                   focus:bg-white focus:outline-none"
                 required
               ></input>
             </div>
 
-            <div class="mt-2">
+            <div class="mt-2 w-full md:w-1/2 px-3">
               <label class="block text-gray-700">State </label>
               <input
                 type="text"
@@ -119,13 +120,13 @@ const SignUp = () => {
                 id=""
                 placeholder="Enter State"
                 minlength="6"
-                class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-purple-500
+                class="w-full px-4 py-1 rounded-lg bg-gray-200 mt-2 border focus:border-purple-500
                   focus:bg-white focus:outline-none"
                 required
               ></input>
             </div>
 
-            <div class="mt-2">
+            <div class="mt-2 w-full md:w-1/3 px-3">
               <label class="block text-gray-700">Country</label>
               <input
                 type="text"
@@ -133,10 +134,11 @@ const SignUp = () => {
                 id=""
                 placeholder="Enter Country"
                 minlength="6"
-                class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-purple-500
+                class="w-full px-4 py-1 rounded-lg bg-gray-200 mt-2 border focus:border-purple-500
                   focus:bg-white focus:outline-none"
                 required
               ></input>
+            </div>
             </div>
 
             </div> 
@@ -152,7 +154,7 @@ const SignUp = () => {
             </button>
           </form>
           <hr class="my-6 border-gray-300 w-full"></hr>
-          <p class="mt-8">
+          <p class="mt-5">
             Already an existing user?{" "}
             <a
               href="#"
@@ -162,20 +164,20 @@ const SignUp = () => {
             </a>
           </p>
 
-          <p class="text-sm text-gray-500 mt-12">
+          <p class="text-sm text-gray-500 mt-6">
             &copy; 2022 - All Rights Reserved.
           </p>
         </div>
       </div>
-      {/*
+      
       <div class="bg-purple-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
             <img
               src="https://images.unsplash.com/photo-1527529482837-4698179dc6ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3870&q=80"
               alt=""
-              class="w-full h-full object-cover"
+              class="w-full h-full object-contain "
             ></img>
           </div>
-          */}
+          
     </section>
   );
 };
