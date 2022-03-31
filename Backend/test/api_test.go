295410f81db69cdcec4603c321e04ae1cbf9d94c
Medha Naik <medha.naik@ufl.edu>
2022-03-31 19:55:02 -0400

    added tests for get party by id, cancel, edit party and attend party

--- Backend/test/api_test.go
+++ Backend/test/api_test.go
@@ -104,7 +104,114 @@ func TestAddAddressAPI(t *testing.T) {
 
 	data := url.Values{}
 	data.Set("party_id", "3")
-	data.Set("surname", "bar")
+	data.Set("addr", "3800 SW 34TH ST 32608")
+	database.Database_setup()
+
+	c.Request, _ = http.NewRequest("POST", "api/addresses", strings.NewReader(data.Encode()))
+	c.Request.Header.Set("X-Forwarded-For", "127.0.0.1")
+
+	controllers.GetAddresses(c)
+	//assert.Equal(t, 200, w.Code)
+	var response gin.H
+	err := json.Unmarshal(w.Body.Bytes(), &response)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, exists := response["message"]
+	fmt.Println("")
+	// Make some assertions on the correctness of the response.
+
+	assert.Equal(t, exists, true)
+}
+
+func TestGetPartyByIDAPI(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+
+	data := url.Values{}
+	data.Set("party_id", "2")
+	database.Database_setup()
+
+	c.Request, _ = http.NewRequest("POST", "api/getParty/3", strings.NewReader(data.Encode()))
+	c.Request.Header.Set("X-Forwarded-For", "127.0.0.1")
+
+	controllers.GetParty(c)
+	//assert.Equal(t, 200, w.Code)
+	var response gin.H
+	err := json.Unmarshal(w.Body.Bytes(), &response)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, exists := response["data"]
+	fmt.Println("")
+	// Make some assertions on the correctness of the response.
+
+	assert.Equal(t, exists, true)
+}
+
+func TestEditPartyAPI(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+
+	data := url.Values{}
+	data.Set("party_id", "3")
+	data.Set("details", "best party ever")
+	database.Database_setup()
+
+	c.Request, _ = http.NewRequest("POST", "api/addresses", strings.NewReader(data.Encode()))
+	c.Request.Header.Set("X-Forwarded-For", "127.0.0.1")
+
+	controllers.GetAddresses(c)
+	//assert.Equal(t, 200, w.Code)
+	var response gin.H
+	err := json.Unmarshal(w.Body.Bytes(), &response)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, exists := response["message"]
+	fmt.Println("")
+	// Make some assertions on the correctness of the response.
+
+	assert.Equal(t, exists, true)
+}
+
+func TestDeletePartyAPI(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+
+	data := url.Values{}
+	data.Set("party_id", "2")
+	data.Set("user_id", "4")
+	database.Database_setup()
+
+	c.Request, _ = http.NewRequest("POST", "api/getParty/3", strings.NewReader(data.Encode()))
+	c.Request.Header.Set("X-Forwarded-For", "127.0.0.1")
+
+	controllers.GetParty(c)
+	//assert.Equal(t, 200, w.Code)
+	var response gin.H
+	err := json.Unmarshal(w.Body.Bytes(), &response)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, exists := response["data"]
+	fmt.Println("")
+	// Make some assertions on the correctness of the response.
+
+	assert.Equal(t, exists, true)
+}
+
+func TestAttendPartyAPI(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+
+	data := url.Values{}
+	data.Set("party_id", "3")
+	data.Set("user_id", "4")
 	database.Database_setup()
 
 	c.Request, _ = http.NewRequest("POST", "api/addresses", strings.NewReader(data.Encode()))
