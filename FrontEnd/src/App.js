765735f417d03e93593dfe5adda3e83bfab1263b
Sanjana Aluri <alurisanjana9@gmail.com>
2022-04-01 19:25:16 -0400

    Fixed bug of App.js

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -36,9 +36,9 @@ function App(props) {
 
   return (
     <EventsProvider>
-        <Router>
-        <Routes>
         <Navbar />
+        <Routes>
+        
           <Route path="/" exact element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/partylist" element={<PartyList />} />
@@ -50,7 +50,7 @@ function App(props) {
           />
           <Route path="/partyDetail/:partyId" element={<PartyDetail />} />
         </Routes>
-        </Router>
+        
     </EventsProvider>
   );
 }
