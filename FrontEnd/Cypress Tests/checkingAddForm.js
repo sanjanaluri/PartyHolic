016af4df317fa489e62736b566c990495f669e9f
19santosh99 <santoshmaturi99@gmail.com>
2022-04-19 16:08:28 -0400

    Modified code on CheckingAddForm cypress test.

--- FrontEnd/Cypress Tests/checkingAddForm.js
+++ FrontEnd/Cypress Tests/checkingAddForm.js
@@ -1,27 +1,25 @@
 describe("render the home page", () => {
-    it("renders correctly", () => {
-      cy.visit("/");
-      cy.wait(2000);
-      cy.get('[href="/partylist"]').click();
-      cy.get('.ml-8').click();
+  it("renders correctly", () => {
+    cy.visit("/");
+    cy.wait(2000);
+    cy.get('[href="/partylist"]').click();
+    cy.get(".ml-8").click();
 
-      
-      cy.get('#Party_name').type('My Gator Party',{force: true})
-      cy.get('#Host_id').type('4',{force: true})
-      cy.get('#Address_id').type('Above 18',{force: true})
-      cy.get('#Description').type("This is My party Description")
-      cy.get('#Start_time').type('2006-01-02T15:04:06-05:00')
-      cy.get('#End_time').type('2006-01-02T15:04:06-05:00')
-      cy.get('#Image_id').type('Image3.jpg')
-      cy.get('#Attendee_count').type('10')
-      cy.get('#Latitude').type(-82.3291)
-      cy.get('#Longitude').type(29.63597)
-      cy.get('.inline-flex').click()
-      cy.get('.inline-block').click()
-      cy.visit("/");
-      cy.reload()
-      cy.wait(1000);
-      cy.get('[href="/partylist"]').click();
-    });
+    cy.get("#Party_name").type("Gator Party", { force: true });
+    cy.get("#Tags").type("above 18");
+    cy.get("#Description").type("This is My party Description");
+    cy.get("#Start_time").type("Jan 2,2006 3:00 pm");
+    cy.get("#End_time").type("Jan 3,2006 4:00 pm");
+    cy.get("#Lane_apt").type("4000 SW 37th Blvd");
+    cy.get("#City").type("Gainesville");
+    cy.get("#State").type("Florida");
+    cy.get("#Country").type("United States of America");
+    cy.get(".py-3 > .ml-8").click();
+    cy.wait(1000);
+    cy.get(".inline-block").click();
+    cy.get(".text-3xl").click();
+    cy.reload();
+    cy.wait(1000);
+    cy.get('[href="/partylist"]').click();
   });
-  
+});
